f4db03f26df669116385cfac57371b5b0e19ec79
Hands-On Lab User <holuser@docker.oracleworld.com>
2017-12-14 14:08:48 -0800

    Updated to support Docker Workshops

--- createUserAlpha.sql
+++ createUserAlpha.sql
@@ -1,5 +1,7 @@
-CREATE USER 'alpha'@'localhost' IDENTIFIED BY 'Alpha2017_';
-GRANT ALL PRIVILEGES ON *.* TO 'alpha'@'localhost' WITH GRANT OPTION;
+create database AlphaOfficeDB;
+use AlphaOfficeDB;
+CREATE USER 'alpha'@'`hostname`' IDENTIFIED BY 'Alpha2017_';
+GRANT ALL PRIVILEGES ON *.* TO 'alpha'@'`hostname`' WITH GRANT OPTION;
 CREATE USER 'alpha'@'%' IDENTIFIED BY 'Alpha2017_';
 GRANT ALL PRIVILEGES ON *.* TO 'alpha'@'%' WITH GRANT OPTION;
 exit
